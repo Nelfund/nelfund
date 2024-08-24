84db09c362612c4356c45c69fe4a52772f2e3a0d
skad12 <stephenkunle26@gmail.com>
2024-08-24 14:10:39 +0100

    state label adjusted

--- components/YourInfo/index.jsx
+++ components/YourInfo/index.jsx
@@ -74,11 +74,11 @@ const YourInfo = () => {
         </div>
 
         <div className="mt-10">
-          <label className="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">
+          <label className="absolute px-2 ml-2 -mt-5 font-medium text-gray-600 bg-white">
             State
           </label>
           <select
-            className="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black"
+            className="block w-full px-4 py-4 -mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black"
             {...register("location", { required: true })}
           >
             <option value="" disabled selected>
